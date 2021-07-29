bd7b0ffe81f0289851424daa113829f6e28c453d
Andri Purnomo <56879512+andripurnomo87@users.noreply.github.com>
2021-07-29 22:18:06 +0700

    rewrite route api

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use("/", indexRouter);
 app.use("/admin", adminRouter);
 
 // router api
-app.use("/api", apiRouter);
+app.use("/api/v1", apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
